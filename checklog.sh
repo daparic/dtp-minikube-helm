4b04c6359ad2cc6621c5aae125e47147f4424237
user1 <user1@DESKTOP-I2RP9ER>
2024-04-02 10:37:49 +0800

    Improve script

--- checklog.sh
+++ checklog.sh
@@ -4,4 +4,11 @@
 alias kubectl='minikube kubectl --'
 
 kubectl get pod -n parasoft-dtp-namespace
-kubectl logs dtp-helm-dtp-pod -n parasoft-dtp-namespace
+while [ $? -ne 0 ];do
+    kubectl get pod -n parasoft-dtp-namespace | grep -w Running 
+    printf "."
+    sleep 5
+done
+# kubectl logs dtp-helm-dtp-pod -n parasoft-dtp-namespace
+
+minikube service dtp-service -n parasoft-dtp-namespace
